b535674cc8162e9e60ea45d74e20a19317b0e7fb
Joe-Demp <dempseyjoseph96@yahoo.com>
2020-03-23 03:50:00 +0000

    Implemented methods in ClassFileDeleter
    Signed-off-by: Joe-Demp <dempseyjoseph96@yahoo.com>

---
+++ src/main/java/exceptions/BadPathException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class BadPathException extends Exception {
+    public BadPathException(String message) {
+        super(message);
+    }
+
+    public BadPathException() {
+        super();
+    }
+}
--- src/main/java/util/ClassFileDeleter.java
+++ src/main/java/util/ClassFileDeleter.java
@@ -1,4 +1,60 @@
 package util;
 
+import exceptions.BadPathException;
+
+import javax.print.Doc;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ */
 public class ClassFileDeleter {
+    private final Path path;
+    private static final DotClassFilenameFilter classFF = new DotClassFilenameFilter();
+    private static final DirectoryFileFilter dirFF = new DirectoryFileFilter();
+
+    /**
+     *
+     * @param path
+     * @return
+     * @throws BadPathException
+     */
+    public static ClassFileDeleter getInstance(Path path) throws BadPathException {
+        File root = path.toFile();
+        if (root.exists()) {
+            if (root.isDirectory()) {
+                return new ClassFileDeleter(path);
+            } else {
+                return new ClassFileDeleter(path.getParent());
+            }
+        }
+        throw new BadPathException("The location specified by the path does not exist");
+    }
+
+    private ClassFileDeleter(Path pathToDirectory) {
+        this.path = pathToDirectory;
+    }
+
+    /**
+     *
+     */
+    public void execute() {
+        Queue<File> directories = new LinkedList<>();
+        File current;
+
+        directories.add(path.toFile());
+        while (!directories.isEmpty()) {
+            current = directories.poll();
+
+            for (File file : current.listFiles(classFF)) {
+                file.deleteOnExit();
+            }
+
+            directories.addAll(Arrays.asList(current.listFiles(dirFF)));
+        }
+    }
 }
