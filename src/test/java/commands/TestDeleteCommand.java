b3de2a62439d3e891e703d7376f62bba6a30961e
Joe-Demp <dempseyjoseph96@yahoo.com>
2020-03-29 18:38:23 +0100

    Added Mockito to build.gradle, and a mock FileSystem to TestDeleteCommand.
    Signed-off-by: Joe-Demp <dempseyjoseph96@yahoo.com>

--- src/test/java/commands/TestDeleteCommand.java
+++ src/test/java/commands/TestDeleteCommand.java
@@ -2,11 +2,16 @@ package commands;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.FileSystem;
+
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
 
 public class TestDeleteCommand {
+    FileSystem mockFS = mock(FileSystem.class);
+
     @Test
-    public void test_1_arg_constructor() {
+    public void test_OneArgConstructor() {
         fail("Not yet implemented");
     }
 }
