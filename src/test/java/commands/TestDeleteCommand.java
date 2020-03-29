b7f5292d535cff5410629494def1654ead68fb9c
Joe-Demp <dempseyjoseph96@yahoo.com>
2020-03-29 03:55:02 +0100

    Added TestDeleteCommand with a failing test
    Signed-off-by: Joe-Demp <dempseyjoseph96@yahoo.com>

---
+++ src/test/java/commands/TestDeleteCommand.java
@@ -0,0 +1,12 @@
+package commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestDeleteCommand {
+    @Test
+    public void test_1_arg_constructor() {
+        fail("Not yet implemented");
+    }
+}
